05c29067e1fe7a56890b9ec0ad91a17cda604f0b
Motakuji13 <65540812+Motakuji13@users.noreply.github.com>
2020-05-19 10:33:07 +0600

    Create 03_Lesson.py

---
+++ 03_Lesson/03_Lesson.py
@@ -0,0 +1,16 @@
+test = 12
+print (test)
+lamar = 12.45
+print (lamar)
+loh = "String"
+print (loh)
+num_1 = int (input ("Enter first num: "))
+num_2 = int (input ("Enter second num: "))
+res = num_1 + num_2
+print ("Result is", res)
+num_3 = input ("Enter third num: ")
+num_4 = float (input ("Enter fourth num: "))
+Res = float (num_3) + num_4
+Res *= 2
+print ("Resultat is", Res)
+name = input("")
