fb4221886ebe3e3ee36504f6d40ea1d432a81437
Motakuji13 <65540812+Motakuji13@users.noreply.github.com>
2020-05-24 13:09:29 +0600

    lesson 5

---
+++ 05_Lesson/05_Lesson_01.py
@@ -0,0 +1,11 @@
+i = 0
+while i <= 10:
+    print (i)
+    i += 2
+
+for j in 'hello world':
+    if j == 'w':
+        continue
+    print (j * 2, end = '')
+
+name = input("")
---
+++ 05_Lesson/05_Lesson_02.py
@@ -0,0 +1,12 @@
+i = 1000
+while i > 100:
+    print (i)
+    i /= 2
+
+for j in 'hello world':
+    if j == 'a':
+        break
+    print (j * 2, end = '')
+else:
+    print ("")
+name = input("Буквы а нет в слове")
